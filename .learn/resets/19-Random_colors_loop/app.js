685c16e194b9b19161b53395c2ded76c63dbf90c
Natali Martinez <163793240+natidevelop@users.noreply.github.com>
2025-02-07 18:30:58 +0000

    practiando

--- .learn/resets/19-Random_colors_loop/app.js
+++ .learn/resets/19-Random_colors_loop/app.js
@@ -16,11 +16,11 @@ function getColor(colorNumber = 0) {
 }
 
 function getAllStudentColors() {
-	for (let i = 0; i < 10; i++) {
-		let exampleColor = getColor(Math.floor(Math.random() * 4) + 1)
-		console.log(exampleColor);
-	}
+
+	//your loop here
+	let exampleColor = getColor(1);
 }
+
 //call the function below with the number of students in the class and print on the console
 getAllStudentColors();
 
